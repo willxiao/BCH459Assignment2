fe11a75b7f1d2b1cf4e1bf22c3d6f677be89ce0f
George <ghodulik95@gmail.com>
2016-03-16 20:14:12 -0400

    Added importer function. Changed data matrix to use an array rather than map. Added data.

--- src/DataImporter.java
+++ src/DataImporter.java
@@ -1,10 +1,48 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class DataImporter {
 	public static DataMatrix<Double> importFromFile(String filename){
+		Scanner s = null;
+		ArrayList<ArrayList<Double>> matrix = new ArrayList<ArrayList<Double>>();
+		
+		try{
+			s = new Scanner(new BufferedReader(new FileReader(filename)));
+			
+			while(s.hasNext()){
+				String line = s.nextLine();
+				String[] vals = line.split(",");
+				
+				ArrayList<Double> row = new ArrayList<Double>();
+				for(int i = 1; i < vals.length; i++){
+					row.add(Double.valueOf(vals[i]));
+				}
+				matrix.add(row);
+			}
+			
+			return new DataMatrix<Double>(matrix);
+		}
+		catch(IOException e){
+			System.out.println(e.getMessage());
+		}
+		finally{
+			if(s!=null){
+				s.close();
+			}
+		}
+		
 		return null;
 	}
 	
 	public static DataMatrix<Integer> convertToDiscreteData(DataMatrix<Double> d){
 		return null;
+	}
+	
+	public static void main(String[] args){
+		DataMatrix<Double> d = importFromFile("scrambled.csv");
+		System.out.println(d.getValue(10, 5));
+		System.out.println(d);
 	}
 }
--- src/DataMatrix.java
+++ src/DataMatrix.java
@@ -5,35 +5,35 @@ import java.util.Map;
 
 public class DataMatrix<T> {
 	private ArrayList<ArrayList<T>> original_matrix;
-	private Map<Integer, Integer> col_map;
+	private int[] col_map;
 	
-	DataMatrix(List<List<T>> m){
+	DataMatrix(ArrayList<ArrayList<T>> m){
 		original_matrix = new ArrayList<ArrayList<T>>(m.size());
 		for(List<T> expression_data : m){
 			original_matrix.add(new ArrayList<T>(expression_data));
 		}
 		
-		col_map = new HashMap<Integer,Integer>();
-		for(int i = 0; i < m.get(0).size(); i++){
-			col_map.put(i, i);
+		col_map = new int[m.get(0).size()];
+		for(int i = 0; i < col_map.length; i++){
+			col_map[i] = i;
 		}
 	}
 	
 	public void changeColumnOrder(int[] colOrder){
 		for(int i = 0; i < colOrder.length; i++){
-			col_map.put(i, colOrder[i]);
+			col_map[i] = colOrder[i];
 		}
 	}
 	
 	public T getValue(int row, int column){
-		return original_matrix.get(row).get(col_map.get(column));
+		return original_matrix.get(row).get(col_map[column]);
 	}
 	
 	public List<T> getRow(int row){
 		List<T> originalRow = original_matrix.get(row);
 		List<T> toReturn = new ArrayList<T>();
 		for(int i = 0; i < originalRow.size(); i++){
-			toReturn.add(originalRow.get(col_map.get(row)));
+			toReturn.add(originalRow.get(col_map[row]));
 		}
 		return toReturn;
 	}
