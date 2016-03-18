da8e42047904f189917d061e49953a3243b7f4fc
George <ghodulik95@gmail.com>
2016-03-17 20:57:14 -0400

    Added support to save results to file.

--- src/Runner.java
+++ src/Runner.java
@@ -1,20 +1,32 @@
 import java.util.Iterator;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 public class Runner {
 	public static final int TEN_FACTORIAL = 3628800;
 	public static final int STEP = TEN_FACTORIAL / 1000;
 	
 	public static void main(String[] args){
-		DataMatrix<Double> original = DataImporter.importFromFile("scrambled.csv");
-		System.out.println("Running Tests on original data");
-		runTests(original);
-		
-		System.out.println("\nRunning Tests on normalized data");
-		DataMatrix<Double> normalized = DataImporter.normalizeDataByRow(original);
-		runTests(normalized);
+		PrintWriter pw = null;
+		try{
+			pw = new PrintWriter("Results.txt");
+			DataMatrix<Double> original = DataImporter.importFromFile("scrambled.csv");
+			pw.println("Running Tests on original data");
+			runTests(original,pw);
+			
+			pw.println("\nRunning Tests on normalized data");
+			DataMatrix<Double> normalized = DataImporter.normalizeDataByRow(original);
+			runTests(normalized,pw);
+		}catch(IOException e){
+			
+		}finally{
+			if(pw != null){
+				pw.close();
+			}
+		}
 	}
 	
-	public static void runTests(DataMatrix<Double> original){
+	public static void runTests(DataMatrix<Double> original,PrintWriter pw){
 		DataMatrix<Double> continuous = original;
 		DataMatrix<Integer> discrete = DataImporter.convertToDiscreteData(continuous);
 		DataMatrix<Double> continuous_q = original;
@@ -63,26 +75,26 @@ public class Runner {
 			}
         }
 		
-		System.out.println("Best Regression Cost: "+bestRegressionCost);
-		System.out.print("Best Order based on regression cost: ");
-		print(bestRegressionOrder);
+		pw.println("Best Regression Cost: "+bestRegressionCost);
+		pw.print("Best Order based on regression cost: ");
+		print(bestRegressionOrder,pw);
 		
-		System.out.println("Best Quadratic Regression Cost: "+bestQRegressionCost);
-		System.out.print("Best Order based on Quadratic Regression cost: ");
-		print(bestQRegressionOrder);
+		pw.println("Best Quadratic Regression Cost: "+bestQRegressionCost);
+		pw.print("Best Order based on Quadratic Regression cost: ");
+		print(bestQRegressionOrder,pw);
 		
-		System.out.println("Best Smooth Cost: "+bestSmoothCost);
-		System.out.print("Best Order based on smoothness cost: ");
-		print(bestSmoothOrder);
+		pw.println("Best Smooth Cost: "+bestSmoothCost);
+		pw.print("Best Order based on smoothness cost: ");
+		print(bestSmoothOrder,pw);
 	}
 	
-	public static void print(int[] a){
+	public static void print(int[] a,PrintWriter pw){
 		if(a==null){
 			return;
 		}
     	for(int i = 0; i < a.length; i++){
-    		System.out.print(a[i]);
+    		pw.print(a[i]);
     	}
-    	System.out.println();
+    	pw.println();
     }
 }
